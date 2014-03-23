befabb4d8b2d7053850c931a3c95e706b2053275
Neil Dingeldein <ndingeldein@gmail.com>
2014-03-22 23:05:01 -0500

    added css that centers logo by default and images to images/

--- php/assets.php
+++ php/assets.php
@@ -1,5 +1,6 @@
 <link rel="icon" href="./images/favicon.ico" type="image/x-icon" /> 
 <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="./images/iui-logo-touch-icon.png">
 
 <link href="./css/normalize.css" rel="stylesheet" type="text/css" />
 <link href="./css/html5bp.css" rel="stylesheet" type="text/css" />
